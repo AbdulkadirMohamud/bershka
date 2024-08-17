8680d0fb6e996810595e8e644794667d1a442ec1
Abdulkadir <qansax30@hotmail.com>
2024-08-18 00:28:57 +0200

    Bershka App

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/asset 1 T-shirt.jpg";
+import product2 from "./assets/asset 2 Hoodie.jpg";
+import product3 from "./assets/asset 3 shirt.jpg";
+import product4 from "./assets/asset 4 baggy.jpg";
+import product5 from "./assets/asset 5 bermuda.jpg";
+import product6 from "./assets/asset 6 straight jeans.jpg";
+import product7 from "./assets/asset 7 shoes1.jpg";
+import product8 from "./assets/asset 8 shoes2.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Boxy fit cropped short sleeve T-shirt",
+    price: 19.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Hoodie",
+    price: 39.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName:  "shirt",
+    price: 45.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Baggy cargo jeans",
+    price: 49.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Denim skater Bermuda shorts",
+   price: 32.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Straight fit jeans",
+    price: 45.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Men’s multi-piece sneakers",
+    price: 69.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Men’s multi-piece sneakers",
+    price: 69.0,
+    productImage: product8,
+  },
+];
